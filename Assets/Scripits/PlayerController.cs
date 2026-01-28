9d92e6cab6f3846de0a0602faf268fe2a5a2ca7f
Eran <michaelieran@gmail.com>
2026-01-28 16:44:52 +0200

    - Introduced new skybox "Cool Cloudy Morning" with associated lighting and material files. - Added texture file for the skybox. - Updated PlayerController to include clamping for player movement. - Modified package dependencies for render pipelines and shader graph. - Adjusted TimeManager settings for improved time management.

--- Assets/Scripits/PlayerController.cs
+++ Assets/Scripits/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] float xClamp = 3f;
+    [SerializeField] float zClamp = 3f;
     [SerializeField] private float moveSpeed = 10f;
     Rigidbody rb;
     Vector2 movementInput;
@@ -29,6 +32,11 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new(movementInput.x, 0f, movementInput.y);
         Vector3 newPosition = currentPostion + moveSpeed * Time.fixedDeltaTime * movement;
 
+        newPosition.x = Mathf.Clamp(newPosition.x, -xClamp, xClamp);
+        newPosition.z = Mathf.Clamp(newPosition.z, -zClamp, zClamp);
+
         rb.MovePosition(newPosition);
     }
+
+
 }
