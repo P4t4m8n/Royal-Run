01b1ccdf49fda4beda711a9d3461f097bde7032a
Eran <michaelieran@gmail.com>
2026-01-28 19:57:03 +0200

    Add StumbleHit animation and implement collision handling with cooldown
    - Added new animation file for StumbleHit.
    - Updated PlayerCollisionHandler to trigger animation on collision with cooldown mechanism.

--- Assets/Scripits/Player/PlayerCollisionHandler.cs
+++ Assets/Scripits/Player/PlayerCollisionHandler.cs
@@ -1,6 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    // private static WaitForSeconds _waitForSeconds1 = new(1f);
+    [SerializeField] private Animator animator;
+    [SerializeField] float collistionCooldown = 1f;
+    float cooldownTimer = 0f;
+    private const string hitStringTrigger = "Hit";
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (cooldownTimer < collistionCooldown) return;
+        animator.SetTrigger(hitStringTrigger);
+        cooldownTimer = 0f;
+    }
+
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     animator.SetTrigger(hitStringTrigger);
+    //     StartCoroutine(ResetTriggerCoroutine());
+    // }
+
+    // private IEnumerator ResetTriggerCoroutine()
+    // {
+    //     yield return _waitForSeconds1;
+    //     animator.ResetTrigger(hitStringTrigger);
+    // }
 
 }
